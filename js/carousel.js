09c43490c8cf8deb1bcfca1cc470372bb6ee3120
Christina <cstrynatka@gmail.com>
2018-03-09 20:02:35 -0500

    Fixed bugs with both carousels so now the controls work for each separately

--- js/carousel.js
+++ js/carousel.js
@@ -1,3 +1,29 @@
+jQuery(document).ready(function($) {
+    $('#myCarousel').carousel({
+            interval: 3000
+    });
+    $('#myCarousel2').carousel({
+            interval: 2000
+    });
+
+    $('#carousel-text').html($('#slide-content-0').html());
+
+    //Handles the carousel thumbnails
+    $('[id^=carousel-selector-]').click( function(){
+            var id_selector = $(this).attr("id");
+            var id = id_selector.substr(id_selector.length -1);
+            var id = parseInt(id);
+            $('#myCarousel').carousel(id);
+    });
+
+
+    // When the carousel slides, auto update the text
+    $('#myCarousel').on('slid', function (e) {
+            var id = $('.item.active').data('slide-number');
+            $('#carousel-text').html($('#slide-content-'+id).html());
+    });
+});
+
 $(document).ready(function() {
   $('#media').carousel({
     pause: true,
@@ -5,7 +31,7 @@ $(document).ready(function() {
   });
 });
 
-$('#carouselExample').on('slide.bs.carousel', function (e) {
+$('#carousel1').on('slide.bs.carousel', function (e) {
 
     var $e = $(e.relatedTarget);
     var idx = $e.index();
